a22ea1a7d606c3404835935b508b0c87221d4816
thamaragerigr <thamaragerigr@gmail.com>
2020-04-03 17:27:51 +0200

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=ABeeZee&family=Fredoka+One&display=swap" },
       { rel: "stylesheet", href: "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" }
 
